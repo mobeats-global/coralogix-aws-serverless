85f23d573b07a384e5b2c111114f85587e02e62d
mvarelamobeats <mvarela@mobeats.io>
2021-12-01 11:15:23 -0300

    deleted blanks

--- src/auto-posture-evaluator/testers/iam_tester.py
+++ src/auto-posture-evaluator/testers/iam_tester.py
@@ -11,19 +11,12 @@ from datetime import date
 class Tester(interfaces.TesterInterface):
     
     def __init__(self):
-        
         self.aws_iam_client = boto3.client('iam')        
-        
         self.aws_iam_resource = boto3.resource('iam')
-
         self.users = self.aws_iam_client.list_users()
-
         self.policies = self.aws_iam_client.list_policies()
-
         self.access_key = self.aws_iam_resource.AccessKey('user_name','id')
-
         self.cache = {}
-
         self.user_id = boto3.client('sts').get_caller_identity().get('UserId')
         self.account_arn = boto3.client('sts').get_caller_identity().get('Arn')
         self.account_id = boto3.client('sts').get_caller_identity().get('Account')
