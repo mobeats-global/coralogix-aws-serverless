da15f8dca75d279c6c9bb301b9df6915334de7d5
mvarelamobeats <mvarela@mobeats.io>
2021-12-02 12:32:28 -0300

    removed type to return

--- src/auto-posture-evaluator/testers/iam_tester.py
+++ src/auto-posture-evaluator/testers/iam_tester.py
@@ -42,7 +42,7 @@ class Tester(interfaces.TesterInterface):
         self.detect_policy_requires_lowercase()
         self.detect_policy_max_password_age()
 
-    def detect_old_access_key(self) -> str:
+    def detect_old_access_key(self):
         result = []
         for user in self.users['Users']:
             days = self.days_between(user['CreateDate'])
@@ -75,7 +75,7 @@ class Tester(interfaces.TesterInterface):
         d2 = date.today()
         return abs((d2 - d1).days)
 
-    def detect_attached_users(self) -> str:
+    def detect_attached_users(self):
         result = []
         for policy in self.policies['Policies']:
             response = self.aws_iam_resource.Policy(policy['Arn'])
